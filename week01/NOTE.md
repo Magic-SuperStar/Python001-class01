7857aff37363bb1ac05e6882f6dd5f32b4fb70f6
ljz <8589273+Magic-SuperStar@users.noreply.github.com>
2020-07-05 01:54:28 +0800

    作业01：requests/BeautifulSoup/lxml.etree.xpath

--- week01/NOTE.md
+++ week01/NOTE.md
@@ -1,1 +1,13 @@
-学习笔记
+学习笔记  
+### maoyan 使用requests/BeautifulSoup/lxml.etree.xpath
+#### 问题1：
+* 模拟浏览器打开<https:maoyan.com/films?showType=2>(想着以为是反爬虫的问题)  
+要headers['user-agent']:'Mozilla/4.0 ......' 用的是本开发电脑的浏览器的user-agent  
+一时可以一时不行
+####  问题2：
+* 使用lxml.etree.xpath的语法问题’div[@class="movie-brief-container"]..‘前面缺少’//‘  
+导致xpath找不到对应元素  
+#### 问题3：
+* lxml.etree.xpath返回的是数组集合  
+出现外循环的数组无法append()  
+问题4：还有就是url拼接问题
--- week01/r_maoyan.py
+++ week01/r_maoyan.py
@@ -1,9 +1,10 @@
 # 安装并使用 requests、bs4 库，爬取猫眼电影（）的前 10 个
 # 电影名称、电影类型和上映时间，并以 UTF-8 字符集保存到 csv 格式的文件中。
+import lxml
 import requests
 from bs4 import BeautifulSoup as bs
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 header = {'user-agent': user_agent}
 
 
@@ -16,16 +17,46 @@ def get_url_name(url):
         # 名字
         my_name = t_info[0].find('span', ).text
         # 类型
-        my_type = t_info[1].find('span', ).nextSibling.replace(" ", "").replace('\n', '')
+        my_type = t_info[1].find('span', ).nextSibling.strip()
         # 日期
-        my_date = t_info[3].find('span', ).nextSibling.replace(" ", "").replace('\n', '')
+        my_date = t_info[3].find('span', ).nextSibling.strip()
         tmp = [my_name, my_type, my_date]
         m_list.append(tmp)
     return m_list;
 
 
-my_list = get_url_name('https://maoyan.com/films?showType=3')
+###### 第二种方法
+
+
+def get_info(t_url):
+    response = requests.get(t_url, headers=header)
+    # xml化处理
+    selector = lxml.etree.HTML(response.text)
+    # 名字
+    my_name = selector.xpath('//div[@class="movie-brief-container"]/h1/text()')
+    # 类型
+    my_type = selector.xpath('//div[@class="movie-brief-container"]/ul/li[1]/a/text()')
+    # 日期
+    my_date = selector.xpath('//div[@class="movie-brief-container"]/ul/li[3]/text()')
+    return [my_name[0], ','.join(my_type), my_date[0]]
+
+
+m_url = 'https://maoyan.com'
+
+
+def get_urls(url):
+    response = requests.get(url, headers=header)
+    i_url = bs(response.text, 'html.parser')
+    m_list = []
+    for tags in i_url.find_all('div', attrs={'class': 'movie-item film-channel'})[:10]:
+        t_url = tags.find('a', ).get('href')
+        m_list.append(get_info(m_url + t_url))
+    return m_list
+
+
 import pandas as pd
 
-my_data = pd.DataFrame(my_list)
-my_data.to_csv('./maoyan.csv', encoding='utf8', index=False, header=False)
+my_list = get_urls('https://maoyan.com/films?showType=3')
+
+my_data = pd.DataFrame(data=my_list)
+my_data.to_csv('./maoyan01.csv', encoding='utf-8', index=False, header=False)
